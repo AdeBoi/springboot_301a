1bde2fc2cbffee0caf758eca2247e6cf4179ac00
adeboi <adebotheentertainer@gmail.com>
2019-03-27 21:29:26 -0400

    Initial commit

---
+++ src/test/java/com/xife/springboot_301a/Springboot301aApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xife.springboot_301a;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Springboot301aApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
